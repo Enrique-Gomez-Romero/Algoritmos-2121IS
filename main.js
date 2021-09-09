09714168f74be8877ed07f9f9b76daa3e76921a8
Enrique-Gomez-Romero <enrique_1321124095@uptecamac.edu.mx>
2021-09-09 12:38:43 -0500

    nuevo

--- main.js
+++ main.js
@@ -10,29 +10,67 @@
     || O
     !  Negación    
 */
-var x = 300; // Variable para comparar
-var y = 20;
-var s = 30;
-if(x < y){
-    console.log(x + " es menor a " + y);
-}else{
-    console.log(x +" es mayor a " + y);
-}
+// var x = 300; // Variable para comparar
+// var y = 20;
+// var s = 30;
+// if(x < y){
+//     console.log(x + " es menor a " + y);
+// }else{
+//     console.log(x +" es mayor a " + y);
+// }
 
-var humano1 = "Emmanuel";
-var humano2 = "Emmanuel";
+// var humano1 = "Emmanuel";
+// var humano2 = "Emmanuel";
 
-if(humano1 == humano2){
-    console.log("Son iguales");
-}else{
-    console.log("Son diferentes");
-}
+// if(humano1 == humano2){
+//     console.log("Son iguales");
+// }else{
+//     console.log("Son diferentes");
+// }
 
-var pi = 3.1416;
-var radio = 10;
+// var pi = 3.1416;
+// var radio = 10;
 
-if(pi != radio){
-    console.log("Son diferentes");
-}else{
-    console.log("Son iguales");
-}
+// if(pi != radio){
+//     console.log("Son diferentes");
+// }else{
+//     console.log("Son iguales");
+// }
+
+
+//var transporte = "carro";
+
+//console.log("Paso 1: Salir a la entrada de mi casa");
+
+//if(transporte == "combi"){
+  //  console.log("Paso 2: Subirme a la combi");
+//}else{
+    //console.log("Paso 2: Subirme al transporte publico");
+//}
+
+//console.log("Paso 3: Bajar en paradero de Tecámac");
+//console.log("Paso 4: Caminar a la entrada de la uni");
+//console.log("Paso 5: Caminar al aula");
+
+var Tortillas = 10;
+var p = "Paso";
+var DineroM = 9;
+var DineroN = 11;
+
+
+console.log(p+"1:Salir de mi casa con el dinero");
+console.log(p+"2:Caminar");
+console.log(p+"3:Llegar a la tienda y pedirlas");
+ if (Tortillas < DineroM ) {
+     console.log("Te regresas feliz de la vida a tu cassa");
+
+     
+ } else {
+     console.log(p+"4 Te regresas a tu casa pensando lo pendejo que estas xD");
+     console.log(p+"5 Regresas a tu casa a Tomar dinero")
+     console.log(p+"6 Tomas el dinero y regresas a la tienda")
+     if (Tortillas < DineroN  ) {
+         console.log("Por fin tienes tus tortillas ")
+         
+     }
+     }
